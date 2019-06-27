d5a9732671c8016678d13d639ce6101c357768db
Mitchhh <michelverheijen@hotmail.com>
2019-06-27 09:34:03 +0200

    Updated profiles for JPA service, tested active profiles. Closes #48

--- pet-clinic-data/src/main/java/guru/springframework/petclinic/services/map/VetMapService.java
+++ pet-clinic-data/src/main/java/guru/springframework/petclinic/services/map/VetMapService.java
@@ -5,11 +5,13 @@ import guru.springframework.petclinic.model.Vet;
 import guru.springframework.petclinic.services.SpecialtyService;
 import guru.springframework.petclinic.services.VetService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
+@Profile({"default", "map"})
 public class VetMapService extends AbstractMapService<Vet, Long> implements VetService {
 
     private final SpecialtyService specialtyService;
